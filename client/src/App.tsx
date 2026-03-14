0e46b6919a983c813d71d71099d0ae14d73ecdbe
antur84 <andreas.turku@gmail.com>
2026-03-14 17:41:26 +0100

    dont wrap h1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div className="mx-auto max-w-2xl p-4">
       <header className="mb-6 flex items-center justify-between">
         <a href="/">
-          <h1 className="text-2xl font-bold">Howler 🐺</h1>
+          <h1 className="text-2xl font-bold text-nowrap">Howler 🐺</h1>
         </a>
         <LoginButton />
       </header>
